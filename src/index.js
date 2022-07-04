7ca965c123464a0adb75f97b0667e251ed06a1ea
levladik <vladislavtyshenko@yandex.ru>
2022-07-04 19:30:44 +0400

    feat: 'add function'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	n = Math.abs(n);
+	let nStr = n.toString();
+	let i = nStr.length - 1;
+   let result = '';
+   while (i >= 0) {
+     result = `${result}${nStr[i]}`;
+     i--;
+  }
+
+   return result; 
 }
